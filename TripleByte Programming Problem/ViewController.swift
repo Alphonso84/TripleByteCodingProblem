442073131c2f43a6de37db924e8f9604b4d3da21
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-12-11 00:23:23 -0800

    Initial Commit

---
+++ TripleByte Programming Problem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripleByte Programming Problem
+//
+//  Created by user on 12/11/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
