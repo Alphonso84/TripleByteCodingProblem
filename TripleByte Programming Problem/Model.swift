19cb3744b82f17710743b1fea3b4168f1bc49647
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-12-11 00:33:12 -0800

    Updates

---
+++ TripleByte Programming Problem/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  TripleByte Programming Problem
+//
+//  Created by user on 12/11/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
---
+++ TripleByte Programming Problem/Networking.swift
@@ -0,0 +1,17 @@
+//
+//  Networking.swift
+//  TripleByte Programming Problem
+//
+//  Created by user on 12/11/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Networking {
+
+var baseURL = "https://chex-triplebyte.herokuapp.com/api/cats?page=0"
+
+
+}
